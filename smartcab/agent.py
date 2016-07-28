fc3496350491fb7961ae109fdde38c5341717a30
Edward Minnett <ed@methodic.io>
2016-07-28 18:04:34 +0100

    Feat: Turn of deadline enforcement when initialising the agent
    As instructed in the project instructions.

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -51,7 +51,7 @@ def run():
     # Set up environment and agent
     e = Environment()  # create environment (also adds some dummy traffic)
     a = e.create_agent(LearningAgent)  # create agent
-    e.set_primary_agent(a, enforce_deadline=True)  # specify agent to track
+    e.set_primary_agent(a, enforce_deadline=False)  # specify agent to track
     # NOTE: You can set enforce_deadline=False while debugging to allow longer trials
 
     # Now simulate it
